d2969258705c58f78d21a1ef4a674c6e5092f449
Kartikey Soni <sonikartikey005@gmail.com>
2023-01-21 12:59:44 -0500

    Updates

---
+++ precache-manifest.efba49a2398f69ca19c9d01bd9bcc263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f7101f26c3b213fe21438f4ec6424e",
+    "url": "/Kartikey_portfolio/index.html"
+  },
+  {
+    "revision": "7cc79901e1f9e2fb55c1",
+    "url": "/Kartikey_portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "7b2fa59a9f5a2442f423",
+    "url": "/Kartikey_portfolio/static/css/main.19c42021.chunk.css"
+  },
+  {
+    "revision": "7cc79901e1f9e2fb55c1",
+    "url": "/Kartikey_portfolio/static/js/2.3ce688f3.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Kartikey_portfolio/static/js/2.3ce688f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2fa59a9f5a2442f423",
+    "url": "/Kartikey_portfolio/static/js/main.22e85e6f.chunk.js"
+  },
+  {
+    "revision": "7b38034bab23c4fb2a27",
+    "url": "/Kartikey_portfolio/static/js/runtime-main.f9927047.js"
+  }
+]);
